9ded66714dabd2c85488599b2693d05d51be74b5
mohamedsalvi <117474871+mohamdeTAreK@users.noreply.github.com>
2023-05-08 14:53:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// for(var i=2;i<=10;i++){
+//     for(var j=2;j<i;j++){
+//         if(i%j==0){
+//                break;
+//         }
+//     }
+//     if(i==j){
+//         document.write(i+"<br>")
+//     }
+// }
+
+
+// alert("غير مسموح بترك الاسم فارغ ");  
+
+// grabing 2 btn
+
